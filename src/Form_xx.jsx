733c891a3a8aa5d219a1642e96d7cf6c3138d3f8
Alex718296 <as71829696@gmail.com>
2024-03-20 21:50:30 +0800

    w05-P4-2

--- src/Form_xx.jsx
+++ src/Form_xx.jsx
@@ -1,10 +1,45 @@
 import { useState } from 'react';
-
+ 
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+ 
+import { supabase } from './db/clientSupabase';
+ 
+import { toast } from 'react-toastify';
+ 
 const Form_xx = () => {
   const [newItemName, setNewItemName] = useState('');
-
+ 
+  const queryClient = useQueryClient();
+  const { mutate: createTask, isLoading } = useMutation({
+    mutationFn: async (title) => {
+      try {
+        const { data, error } = await supabase
+          .from('task_69')
+          .insert([{ title, is_done: false }])
+          .select();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tasks'],
+      });
+      toast.success('task added');
+      setNewItemName('');
+    },
+    onError: (error) => {
+      toast.error(error.response.data);
+    },
+  });
+ 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (newItemName === '') {
+      toast.warning('Please Provide title');
+    } else {
+      createTask(newItemName);
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
